1fb0d697960ffe1817c2e5e4d9b54ff179ed9b64
Viktoriia <pecourses>
2024-01-21 15:02:12 +0200

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { STATIC_PATH } = require('./constants');
+const { errorHandlers } = require('./middleware');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.static(STATIC_PATH));
+
+app.use(express.json());
+
+app.use('/api', router);
+
+app.use(errorHandlers.dbErrorHandler, errorHandlers.errorHandler);
+
+module.exports = app;
+
+// Add endpoints handlers
+
+// user 1:n task
+
+// POST /api/users {}
+// GET /api/users
+// GET /api/users/1
+// PATCH /api/users/1 {}
+// DELETE /api/users/1
+
+// отримати задачі користувача
+// GET /api/users/1/tasks
+
+// GET /api/tasks (+user)
